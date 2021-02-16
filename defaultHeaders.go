f4fdd7464c5cb1df83b6f21efd9a7d7c5af4a151
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-02-15 21:38:30 -0800

    Moving default headers to gog_urls

--- defaultHeaders.go
+++ defaultHeaders.go
@@ -4,32 +4,15 @@
 
 package gog_auth_urls
 
-import "net/http"
-
-func addDefaultHeaders(req *http.Request, host string) {
-	const (
-		acceptHeader         = "text/html"
-		acceptLanguageHeader = "en-us"
-		connectionHeader     = "keep-alive"
-		userAgentHeader      = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) " +
-			"AppleWebKit/537.36 (KHTML, like Gecko) " +
-			"Chrome/87.0.4280.141 " +
-			"Safari/537.36 " +
-			"Edg/87.0.664.75" // Microsoft Edge 87 UA string
-	)
-	if host != "" {
-		req.Host = host
-	}
-	req.Header.Set("Accept", acceptHeader)
-	req.Header.Set("Accept-Language", acceptLanguageHeader)
-	req.Header.Set("Connection", connectionHeader)
-	req.Header.Set("User-Agent", userAgentHeader)
-}
+import (
+	"github.com/arelate/gog_urls"
+	"net/http"
+)
 
 func AddAuthHostDefaultHeaders(req *http.Request) {
-	addDefaultHeaders(req, authHost)
+	gog_urls.SetDefaultHeaders(req, authHost, gog_urls.AcceptTextHTML)
 }
 
 func AddLoginHostDefaultHeaders(req *http.Request) {
-	addDefaultHeaders(req, loginHost)
+	gog_urls.SetDefaultHeaders(req, loginHost, gog_urls.AcceptTextHTML)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/arelate/gog_auth_urls
 
 go 1.15
 
-require github.com/arelate/gog_urls v0.1.4-alpha
+require github.com/arelate/gog_urls v0.1.6-alpha
